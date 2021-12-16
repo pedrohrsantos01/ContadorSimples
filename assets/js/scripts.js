49c3c81751e4aba7f26bc5e8b451a2f7abd18670
Pedro Santos <pedrohrsantos01@gmail.com>
2021-12-16 11:15:37 -0300

    funcionalidades adicionais

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,23 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
+var button = document.getElementsByTagName('button')
+
 
 function increment() {
     currentNumber++;
     currentNumberWrapper.innerHTML = currentNumber
+    if(currentNumber >= 10) {
+        button.namedItem('adicionar').style.display = 'none'
+    }
 }
 
 function decrement() {
     currentNumber--;
     currentNumberWrapper.innerHTML = currentNumber
+    if(currentNumber <= 0) {
+        currentNumberWrapper.style.color = 'red'
+        button.namedItem('subtrair').style.display = 'none'
+    }
 }
 
 
